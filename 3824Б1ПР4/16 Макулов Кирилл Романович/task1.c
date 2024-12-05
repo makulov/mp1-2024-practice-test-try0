c56a0814253efd782a74e22bc0f7a23ab19b386d
makulov <makulov.kurull@gmail.com>
2024-12-05 09:46:35 +0300

    Update task1.c

--- 3824Б1ПР4/16 Макулов Кирилл Романович/task1.c
+++ 3824Б1ПР4/16 Макулов Кирилл Романович/task1.c
@@ -1,7 +1,23 @@
-// Задача 1. Найти сумму целых чисел a и b.
-// Если a + b >= INT_MAX, возвращать INT_MAX
-// Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <stdio.h>
+#include "sum.h"
+
+int sum_of_two_numbers(int a, int b) {
+    if (a > 0 && b > 0 && a > INT_MAX - b) {
+        return INT_MAX; // Переполнение при сложении положительных чисел
+    }
+    if (a < 0 && b < 0 && a < INT_MIN - b) {
+        return INT_MIN; // Переполнение при сложении отрицательных чисел
+    }
+    return a + b; // Возврат суммы в остальных случаях
+}
+
+int main() {
+    int a = 2147483647; // INT_MAX
+    int b = 1;
+    
+    int result = sum_of_two_numbers(a, b);
+    
+    printf("Сумма %d и %d равна: %d\n", a, b, result);
+    
+    return 0;
 }

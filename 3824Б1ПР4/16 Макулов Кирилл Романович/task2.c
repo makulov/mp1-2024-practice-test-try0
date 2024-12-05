5d5750893353b6618fb9ccc410da5730d5e72eea
makulov <makulov.kurull@gmail.com>
2024-12-05 09:50:16 +0300

    Update task2.c

--- 3824Б1ПР4/16 Макулов Кирилл Романович/task2.c
+++ 3824Б1ПР4/16 Макулов Кирилл Романович/task2.c
@@ -1,6 +1,15 @@
-// Задача 2. Найти произведение натуральных чисел a и b
-// Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  return 0;
+#include <limits.h> // Для UINT_MAX
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    // Проверяем, не произойдет ли переполнение
+    if (a == 0 || b == 0) {
+        return 0; // Если одно из чисел 0, возвращаем 0
+    }
+    
+    // Проверка на переполнение
+    if (a > UINT_MAX / b) {
+        return UINT_MAX; // Если a * b > UINT_MAX, возвращаем UINT_MAX
+    }
+    
+    return a * b; // Возвращаем произведение
 }
